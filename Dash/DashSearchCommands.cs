f7132cafea255eda09436480882bdb9cdd6c6444
Derek Foreman <dforeman@bloommg.com>
2016-01-26 20:24:37 -0500

    initial import.

---
+++ Dash/DashSearchCommands.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Dash
+{
+	public enum DashSearchCommands
+	{
+	}
+}
+
---
+++ Dash/Properties/AddinInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Mono.Addins;
+using Mono.Addins.Description;
+
+[assembly:Addin (
+	"Dash", 
+	Namespace = "Dash",
+	Version = "1.0"
+)]
+
+[assembly:AddinName ("Dash")]
+[assembly:AddinCategory ("IDE extensions")]
+[assembly:AddinDescription ("Dash")]
+[assembly:AddinAuthor ("Derek Foreman")]
+
